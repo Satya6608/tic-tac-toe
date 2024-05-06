e0e52d3a50e9cd98098b18956658b55016c35554
Satya <satyaprakash6608@gmail.com>
2024-05-06 12:51:24 +0530

    res not defined fixed

--- server/server.js
+++ server/server.js
@@ -47,7 +47,9 @@ if (process.env.NODE_ENV === "production") {
 
   // app.get("*", (req, res) =>
   //   res.sendFile(path.resolve(__dirname1, "client", ".output/server", "index.mjs"))
-  res.send("API is running..");
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
   // );
 } else {
   app.get("/", (req, res) => {
