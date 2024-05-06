30812748b597248f1c41f519d245868fa38a3188
Satya <satyaprakash6608@gmail.com>
2024-05-06 14:00:46 +0530

    tailwind remove

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: ["@pinia/nuxt"],
   plugins: [
     { src: '~/plugins/sessionStoragePlugin.js', mode: 'client'}, // Register the plugin
   ]
